3d74f62e281be8339311e1bae6bb4294ce4e6e81
Voloshin Konstantin <23.03@i.ua>
2018-09-22 13:47:51 +0300

    edits

--- src/scripts/common/calendar.js
+++ src/scripts/common/calendar.js
@@ -10,19 +10,16 @@ $(document).ready(function() {
                 $('.calendar__row--hiden').toggleClass('active');
                 $('.tooltip').removeClass('scale');
             });
-        /*if(document.documentElement.clientWidth < 1200) { 
-            $('.calendar__date.active').mousemove(function(){
-                $('.tooltip--laptop').css('transform', 'scale(1)');
-            });
+        if(document.documentElement.clientWidth < 1200) {
 
-            $('.calendar__date.active').mouseout(function(){
-                $('.tooltip--laptop').css('transform', 'scale(0)');
-            });
-        };*/
-
-        if(document.documentElement.clientWidth < 769) { 
             $('.calendar__date.active').on('click', function(){
+                var $this = $(this);
                 event.preventDefault();
+                $('.tooltip.scale').removeClass('scale');
+
+                window.setTimeout(function() {
+                    $($this).next().toggleClass('scale');
+                }, 300);
             });
         };
 
